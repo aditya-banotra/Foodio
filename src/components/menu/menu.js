607d35802938102b7a6c639f7f0510653b4ebe19
aditya3wh <aditya.banotra@wherehouse.io>
2023-07-23 23:19:24 +0530

    Initial Commit

---
+++ src/components/menu/menu.js
@@ -0,0 +1,97 @@
+import { Button, Table, notification } from "antd";
+import React, { useEffect, useState } from "react";
+import { columns, components, useComumns } from "./utils";
+import axios from "axios";
+
+export function Menu() {
+  const [dataSource, setDataSource] = useState(null);
+  const [prices, setPrices] = useState([]);
+  function onSave() {
+    localStorage.setItem("foodioPrice", JSON.stringify(prices));
+    notification.success({
+        message: 'Prices Saved',
+        description: 'prices were saved',
+        placement: 'top'
+    });
+  }
+  function onReset() {
+    const prices = localStorage.getItem("foodioPrice");
+    const priceArray = JSON.parse(prices);
+    setPrices(priceArray);
+    notification.success({
+        message: 'Prices Reset',
+        description: 'prices were reset',
+        placement: 'top'
+    });
+  }
+  async function getData() {
+    const { data } = await axios.get(
+      "https://foodiobe.onrender.com"
+    );
+
+    const prices = localStorage.getItem("foodioPrice");
+    if (prices) {
+      const priceArray = JSON.parse(prices);
+      setPrices(priceArray);
+    } else {
+      const prices = data.map((item) => {
+        return { id: item.id, price: item.price };
+      });
+      localStorage.setItem("foodioPrice", JSON.stringify(prices));
+      setPrices(prices);
+    }
+
+    setDataSource(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleSave = (row) => {
+    const newData = [...prices];
+    console.log(row);
+    const index = newData.findIndex((item) => row.id === item.id);
+    const item = newData[index];
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    });
+    setPrices(newData);
+  };
+
+  const finalColumns = useComumns({ prices, setPrices }).map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave,
+      }),
+    };
+  });
+
+  return (
+    <>
+      <div
+        style={{ display: "flex", justifyContent: "flex-end", margin: "1rem" }}
+      >
+        <Button style={{ marginRight: "1rem" }} onClick={onSave}>
+          Save
+        </Button>
+        <Button onClick={onReset}>Reset</Button>
+      </div>
+      <Table
+        components={components}
+        rowClassName={() => "editable-row"}
+        columns={finalColumns}
+        dataSource={dataSource}
+      />
+    </>
+  );
+}
---
+++ src/components/menu/utils.js
@@ -0,0 +1,144 @@
+import { Button, Form, Input } from "antd";
+import React, { useContext, useEffect, useRef, useState } from "react";
+const EditableContext = React.createContext(null);
+
+export function useComumns({ prices, setPrices }) {
+  return [
+    {
+      title: "category",
+      dataIndex: "category",
+      key: "category",
+    },
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+      render: (url) => {
+        return <>{ url && <img style={{ height: "50px", width: "50px" }} src={url}></img> } </>;
+      },
+    },
+    {
+      title: "label",
+      dataIndex: "label",
+      key: "label",
+    },
+    {
+      title: "description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+      render: (price, data) => {
+        return (
+          <> {data?.id && <> {prices.filter((p) => p.id === data.id)[0].price}</>}</>
+        );
+      },
+      editable: prices,
+    },
+  ];
+}
+
+const EditableCell = ({
+  title,
+  editable,
+  filteredValue,
+  children,
+  dataIndex,
+  record,
+  handleSave,
+  ...restProps
+}) => {
+  const [editing, setEditing] = useState(false);
+  const inputRef = useRef(null);
+  const form = useContext(EditableContext);
+
+  useEffect(() => {
+    if (editing) {
+      inputRef.current.focus();
+    }
+  }, [editing]);
+
+  const toggleEdit = () => {
+    setEditing(!editing);
+    if (editable) {
+      form.setFieldsValue({
+        [dataIndex]: editable.filter((i) => i.id === record[dataIndex]),
+      });
+    }
+  };
+
+  const save = async () => {
+    try {
+      const values = await form.validateFields();
+      toggleEdit();
+      handleSave({
+        ...record,
+        ...values,
+      });
+    } catch (errInfo) {
+      console.log("Save failed:", errInfo);
+    }
+  };
+
+  let childNode = children;
+  if (editable) {
+    childNode = editing ? (
+      <Form.Item
+        style={{
+          margin: 0,
+        }}
+        name={dataIndex}
+        rules={[
+          {
+            required: true,
+            message: `${title} is required.`,
+          },
+        ]}
+      >
+        <Input ref={inputRef} onBlur={save} onPressEnter={save} />
+      </Form.Item>
+    ) : (
+      <div
+        className="editable-cell-value-wrap"
+        style={{
+          paddingRight: 24,
+        }}
+        onClick={toggleEdit}
+      >
+        {children}
+      </div>
+    );
+  }
+  return <td {...restProps}>{childNode}</td>;
+};
+
+export const EditableRow = ({ index, ...props }) => {
+  const [form] = Form.useForm();
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  );
+};
+
+export const components = {
+  body: {
+    row: EditableRow,
+    cell: EditableCell,
+  },
+};
